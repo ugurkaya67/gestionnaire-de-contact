2f1a9fd4c2027d863dd31e313c927e9b6b630df1
Ugur <ugurkaya67@hotmail.com>
2024-11-27 16:04:01 +0100

    v0.0 : Ajout initial du projet

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!-- resources/views/welcome.blade.php -->
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestionnaire de Contact</title>
+</head>
+<body>
+    <h1>Bienvenue sur le Gestionnaire de Contacts</h1>
+    <p>Voici une simple page pour commencer avec Laravel.</p>
+</body>
+</html>
