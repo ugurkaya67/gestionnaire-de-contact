dceac501d9657407028525ff84832a7c0f0bc277
Ugur <ugurkaya67@hotmail.com>
2024-12-04 16:02:33 +0100

    v5.0 : Stylisation du tableau avec coins arrondis pour les lignes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestionnaire de Contacts</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-B4gt1jrGC7Jh4AgTPSJzo0cDLch1uwnq0a3P9xzU9z5I5JH37Pq2+Jji3t/pf3dm" crossorigin="anonymous">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
 </head>
 <body>
     <div class="container mt-4">
